18596a29789a58b7f097b3ce288aecfd38ede245
xahidbuffon <xahid.buffon@gmail.com>
2019-09-20 13:49:53 -0500

    nets

--- nets/SRDRM.py
+++ nets/SRDRM.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """
 # > Proposed SRDRM-GAN model 
-#    - Paper: https://arxiv.org/pdf/1903.09766.pdf
+#    - Paper: https://arxiv.org/pdf/x.y.pdf
 #
 # Maintainer: Jahid (email: islam034@umn.edu)
 # Interactive Robotics and Vision Lab (http://irvlab.cs.umn.edu/)
